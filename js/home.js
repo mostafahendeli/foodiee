03318e35fd81846e8c184a835f410826ec6ae196
mostafa-hendeli <mostafahendeli@yahoo.com>
2022-06-01 16:48:33 +0430

    add js-file

---
+++ js/home.js
@@ -0,0 +1,5 @@
+// const toggler = document.querySelector(".menu-icon");
+// const navbar = document.querySelector(".toggler-menu");
+// toggler.addEventListener("click", (e) => {
+//     navbar.classList.toggle("open-menu");
+// });
